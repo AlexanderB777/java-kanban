985ce466b7af14b8289757f4efe268be2adf15c8
AlexanderB777 <v1997ba@yandex.ru>
2024-03-06 20:52:15 +0300

    Расширен функционал HistoryManager Снятие ограничений с длинны истории просмотров Добавлен функционал удаления повторных просмотров

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Поехали!");
         TaskManager manager = Managers.getDefault();
+
     }
 }
